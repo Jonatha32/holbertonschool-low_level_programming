f575fdbedc0165789bfda6b01f8229e8ffd48f13
Jonatha32 <jonaperez038@gmail.com>
2024-06-12 15:31:25 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("Size of a: %lu bytes\n", sizeof(char));
+  printf("Size of a: %lu bytes\n", sizeof(int));
+  printf("Size of a: %lu bytes\n", sizeof(float));
+  printf("Size of a: %lu bytes\n", sizeof(double));
+  return(0);
+}
