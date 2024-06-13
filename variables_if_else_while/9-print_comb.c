092f04a14376556e9f50f3a97da50ee55f527c28
Jonatha32 <jonaperez038@gmail.com>
2024-06-13 17:33:42 +0000

    9-print_com

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void) {
+  int a = 0;
+
+  for (a = 0; a < 10; a++)
+    {
+      putchar(a + '0');
+      if (a < 9)
+	{
+	  putchar(',');
+	  putchar(' ');
+	    }
+      }
+  return 0;
+}
