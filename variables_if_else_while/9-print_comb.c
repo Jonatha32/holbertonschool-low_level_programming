88da7d7c1811e6bba2f6b6c6864758894589b323
Jonatha32 <jonaperez038@gmail.com>
2024-06-13 19:45:16 +0000

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
-int main(void) {
-  int a = 0;
-
-  for (a = 0; a < 10; a++)
-    {
-      putchar(a + '0');
-      if (a < 9)
-	{
-	  putchar(',');
-	  putchar(' ');
-	    }
-      }
-  putchar('\n');
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+if (a < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
