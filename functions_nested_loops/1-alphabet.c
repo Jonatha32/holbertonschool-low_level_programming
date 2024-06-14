3c5ed0875e5f641a052e1b26f0afa8f0d295235b
Jonatha32 <jonaperez038@gmail.com>
2024-06-14 16:39:52 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - entry point
+ *Return: 0 Always Success
+ */
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar ('\n');
+return (0);
+}
