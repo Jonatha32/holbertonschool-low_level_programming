4ed9d967163ec48e29f185b200c85c3cc26462ac
Jonatha32 <jonaperez038@gmail.com>
2024-06-14 20:59:03 +0000

    6-abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value
+ * @a: parameter
+ * Return: a / -a / 0
+ */
+int _abs(int a)
+{
+while (a >= 0)
+{
+return (a);
+}
+while (a <= 0)
+{
+return (-a);
+}
+return (0);
+}
