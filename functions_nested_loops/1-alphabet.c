431daff819e42ff463f53526899c66f23ca3c0ff
Jonatha32 <jonaperez038@gmail.com>
2024-06-14 17:00:28 +0000

    1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *main - entry point
- *Return: 0 Always Success
+ * print_alphabet - print
+ * Return: void
  */
 void print_alphabet(void)
 {
@@ -12,5 +12,4 @@ _putchar(a);
 a++;
 }
 _putchar ('\n');
-return (0);
 }
