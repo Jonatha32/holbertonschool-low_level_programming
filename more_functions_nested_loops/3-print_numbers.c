e307b9e9ad45bd3acacda937af1d5748ac2d7863
Jonatha32 <jonaperez038@gmail.com>
2024-06-15 23:44:53 +0000

    betty style

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,14 @@
 #include "main.h"
+/**
+ * print_numbers - print numbers
+ */
 void print_numbers(void)
 {
-  int a = 0;
-  while (a <= 9)
-    {
-      _putchar(a + '0');
-      a++;
-    }
-  _putchar('\n');
+int a = 0;
+while (a <= 9)
+{
+_putchar(a + '0');
+a++;
+}
+_putchar('\n');
 }
