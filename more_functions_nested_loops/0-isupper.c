060b6783576898b146eca9964b424d958369f0b6
Jonatha32 <jonaperez038@gmail.com>
2024-06-15 22:43:23 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check the upper character
+ * @c: parameter
+ * Return: 1 / 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
