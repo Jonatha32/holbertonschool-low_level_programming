ff81d8b5b55d4fdf92db5de4e148a89dc76b6f50
Jonatha32 <jonaperez038@gmail.com>
2024-06-16 00:30:36 +0000

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ */
+void more_numbers(void)
+{
+int a;
+int  b;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b >= 10)
+_putchar('1');
+_putchar(b % 10 + '0');
+}
+_putchar('\n');
+}
+}
