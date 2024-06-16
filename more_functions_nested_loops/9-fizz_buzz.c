0fb4475b624ff35146445f1a6d325627f14610bb
Jonatha32 <jonaperez038@gmail.com>
2024-06-16 17:17:03 +0000

    9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0 Always Success
+ */
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", a);
+}
+}
+printf("\n");
+return (0);
+}
