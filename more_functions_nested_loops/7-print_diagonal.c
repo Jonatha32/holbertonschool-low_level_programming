8c2e144eddedd8b71af3a4bfc7d7c34b052d16b5
Jonatha32 <jonaperez038@gmail.com>
2024-06-16 02:38:43 +0000

    print

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: parameter
+ */
+void print_diagonal(int n)
+{
+int a;
+int b;
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
