0a55bee641f44a4abceaf971066e7bd9cf740aa5
Jonatha32 <jonaperez038@gmail.com>
2024-06-19 16:54:46 +0000

    c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "main.h"
+/**
+ * reset_to_98 - pointer
+ * @n: Pointer
+ */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap integers
+ * @a: parameter
+ * @b: parameter
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
