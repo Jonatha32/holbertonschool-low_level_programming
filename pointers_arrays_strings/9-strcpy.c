a4af454d455c848f5d3ed7fd539bc678014d6fb4
Jonatha32 <jonaperez038@gmail.com>
2024-06-20 19:28:36 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,8 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int a;
 char *b = dest;
-while (src[a] != '\0')
-{
-*dest = src[a];
-dest++;
-a++;
-}
-*dest = '\0';
+while ((*dest++ = *src++))
+;
 return (b);
 }
