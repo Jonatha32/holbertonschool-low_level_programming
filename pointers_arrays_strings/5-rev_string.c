8897c10aa2adcb65b3265cddd7f02eac3b76d9f5
Jonatha32 <jonaperez038@gmail.com>
2024-06-20 21:05:50 +0000

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,21 @@
 #include "main.h"
+/**
+ * rev_string - rev string
+ * @s: parameter
+ */
 void rev_string(char *s)
-  {
+{
 int a = 0;
 int b;
+char c;
 while (s[a] != '\0')
 {
 a++;
 }
-for (b = a - 1; b >= 0; b--)
+for (b = 0; b < a / 2; b++)
 {
-_putchar(s[b]);
+c = s[b];
+s[b] = s[a - 1 - b];
+s[a - 1 - b] = c;
 }
-_putchar('\n');
 }
