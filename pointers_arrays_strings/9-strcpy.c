13ab083a4dc654911c33719a3cc3610960ce0f44
Jonatha32 <jonaperez038@gmail.com>
2024-06-20 15:13:02 +0000

    9-s

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *_strcpy(char *dest, char *src)
+{
+  int a;
+  int b;
+  while (src[a] != '\0')
+    {
+      a++;
+    }
+  for (b = a - 1; b < a - 1; b++)
+    {
+      _putchar(src[b]);
+    }
+  _putchar(src[b]);
+  return(dest);
+}
+  
