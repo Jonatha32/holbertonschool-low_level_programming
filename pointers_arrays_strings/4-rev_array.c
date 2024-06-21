5626f675b2b6aff3c031395bf86e627e41ca7318
Jonatha32 <jonaperez038@gmail.com>
2024-06-21 13:08:00 +0000

    4-rev

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,9 +7,12 @@
 void reverse_array(int *a, int n)
 {
 int b = 0;
-int c = 0;
-
+int c;
+for (b = 0; b < n / 2; b++)
+{
 c = a[b];
-a[b] = a [n - b - 1];
-a[n - b - 1] = c;
+a [b] = a[n - b - 1];
+a [n - b - 1] = c;
 }
+}
+
