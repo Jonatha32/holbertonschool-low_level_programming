0e072a50b4348228f3ddea5d8ab95c0d7f8dfeee
Jonatha32 <jonaperez038@gmail.com>
2024-06-22 21:07:41 +0000

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,31 @@
 #include "main.h"
+/**
+ * cap_string - string upper
+ * @a: parameter
+ * Return: a
+ */
 char *cap_string(char *a)
 {
-  char *b = a;
-
-  while (*b != '\0')
-    {
-      if (*b == 46 && *b == ' ')
-	{
-	  *b = *b - 32;
-	}
-      a++;
-	}
-  return a;
+char *b = a;
+int c = 1;
+while (*b != '\0')
+{
+if (*b == ' ' || *b == '\t' || *b == '\n' || *b == ',' || *b == ';' ||
+*b == '.' || *b == '!' || *b == '?' || *b == '"' || *b == '(' ||
+*b == ')' || *b == '{' || *b == '}')
+{
+c = 1;
+}
+else if (c && *b >= 'a' && *b <= 'z')
+{
+*b = *b - 32;
+c = 0;
+}
+else
+{
+c = 0;
+}
+b++;
+}
+return (a);
 }
