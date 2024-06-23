19a05f3009baf6c9105a61b7295dc5948c7082e3
Jonatha32 <jonaperez038@gmail.com>
2024-06-23 19:45:32 +0000

    2-str

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - string character
+ * @s: parameter
+ * @c: parameter
+ * Return: s / NULL
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return ('\0');
+}
