4d77f45128d3cae56a40e37cd94fa03c11075b94
Jonatha32 <jonaperez038@gmail.com>
2024-06-25 21:21:46 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,36 @@
 #include "main.h"
+/**
+ * _atoi - string to a int
+ * @s: parameter
+ * Return: b
+ */
 int _atoi(char *s)
 {
-  int a = 1;
-  int b = 0;
-  int c = 0;
+int a = 1;
+int b = 0;
+int c = 0;
 
-    if (s[0] == '-')
-      {
-	a = -1;
-	c++;
-      }
-  if (s[0] == '+')
-    {
-      c++;
-    }
-  while (s[c] != '\0')
-    {
-      if (s[c] >= '0' && s[c] <= '9')
-	{
-	  b = b * 10 + (s[c] - '0');
-	}
-      else
-	{
-	  break;
-	}
-      c++;
-    }
-  b = b * a;
-
-  return b;
+if (s[0] == '-')
+{
+a = -1;
+c++;
+}
+if (s[0] == '+')
+{
+c++;
+}
+while (s[c] != '\0')
+{
+if (s[c] >= '0' && s[c] <= '9')
+{
+b = b * 10 + (s[c] - '0');
+}
+else
+{
+break;
+}
+c++;
+}
+b = b *a;
+return (b);
 }
