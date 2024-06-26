96c9b6ad6f4ee9b94572877b7cbe78383d8d4f7b
Jonatha32 <jonaperez038@gmail.com>
2024-06-26 17:49:46 +0000

    m

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,8 +5,13 @@
 */
 void _puts_recursion(char *s)
 {
-if (s[0] != '\0')
+if (s[0] == '\0')
 {
-_puts_recursion(s + 1);
+  _putchar('\n');
+  return;
+ }
+ _putchar(*s);
+ s++;
+_puts_recursion(s);
 }
-}
+
