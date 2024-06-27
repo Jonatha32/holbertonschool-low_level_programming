83dba0b8ff73ae3c20c8c1b9537bc209cc401f44
Jonatha32 <jonaperez038@gmail.com>
2024-06-27 02:21:44 +0000

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - main function
+ * _sqrt_oki - new function
+ * @n: parameter
+ * @a: parameter
+ * Return: n / a
+ */
+int _sqrt_oki(int n, int a)
+{
+if (a * a == n)
+{
+return (a);
+}
+if (a * a > n)
+{
+return (-1);
+}
+return (_sqrt_oki(n, a + 1));
+}
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0 || n == 1)
+{
+return (n);
+}
+return (_sqrt_oki(n, 1));
+}
