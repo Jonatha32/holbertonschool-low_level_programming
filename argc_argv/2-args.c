5802880f2b5e309512535e79e435e1135621494d
Jonatha32 <jonaperez038@gmail.com>
2024-06-27 14:55:16 +0000

    arguments lol

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int a;
+for (a = 0; a < argc; a++)
+{
+printf("%s\n", argv[a]);
+}
+return (0);
+}
