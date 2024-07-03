ef0169beda0e6c26b9fd0dd35edea4b31ceee124
Jonatha32 <jonaperez038@gmail.com>
2024-07-03 21:45:20 +0000

    free

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_grid - function
+ * @grid: parameter
+ * @height: parameter
+ */
 void free_grid(int **grid, int height)
 {
 int a;
@@ -9,4 +14,3 @@ for (a = 0; a < height; a++)
 free(grid[a]);
 free(grid);
 }
-  
