cf85a0ce75625b9b7299d6e52c617988d07feed9
Jonatha32 <jonaperez038@gmail.com>
2024-07-04 22:54:39 +0000

    doggy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,25 @@
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+#ifndef DOGGY
+#define DOGGY
+
+/**
+ * struct dog - dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+
+typedef struct dog dog_8;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
