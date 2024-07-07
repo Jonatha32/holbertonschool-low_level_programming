aad916f019e93de2fa7ce0ff1d5e5ba070c79169
Jonatha32 <jonaperez038@gmail.com>
2024-07-07 23:38:29 +0000

    printname

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
 #include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name
+ * @name: parameter
+ * @f: parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
+}
