b58961555fea5e58e6f33d3dc25ae135afb1d07b
Jonatha32 <jonaperez038@gmail.com>
2024-07-08 22:21:33 +0000

    index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - index
+ * @array: parameter
+ * @size: parameter
+ * @cmp: parameter
+ * Return: -1 or a
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int a = 0;
+if (size <= 0)
+{
+return (-1);
+}
+while (a < size)
+{
+cmp(array[a]);
+a++;
+if (cmp(array[a]) != 0)
+{
+return (a);
+}
+}
+return (-1);
+}
