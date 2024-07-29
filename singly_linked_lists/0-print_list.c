740a47e4221749a53192b3e87178c5bae487c9d4
Your Name <you@example.com>
2024-07-29 17:23:11 +0000

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,9 +2,21 @@
 #include <stdio.h>
 #include <stddef.h>
 /**
- *
+ * print_list - print list
+ * @h: parameter
+ * Return: a
  */
 size_t print_list(const list_t *h)
 {
-  
+int a = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+a++;
+}
+return (a);
 }
