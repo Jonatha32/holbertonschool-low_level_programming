28d5172da2f4d004cc8897fe6f9c551714d83eee
Jonatha32 <jonaperez038@gmail.com>
2024-07-31 22:26:11 +0000

    task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * sum_dlistint - sum list
+ * @head: parameter
+ * Return: b
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int a = 0;
+dlistint_t *b = head;
+while (b != NULL)
+{
+a += b->n;
+b = b->next;
+}
+return (a);
+}
