518f73c57f036733f968d1170b33ac865016c695
Jonatha32 <jonaperez038@gmail.com>
2024-08-03 22:06:53 +0000

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,10 +1,42 @@
 #include "main.h"
+#include <fcntl.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
- *
+ * read_textfile - read file
+ * @filename: parameter
+ * @letters: parameter
+ * Return: 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  
+int a, b, c;
+char *d;
+if (!filename)
+return (0);
+a = open(filename, O_RDONLY);
+if (a == -1)
+return (0);
+d = malloc(letters);
+if (!d)
+{
+close(a);
+return (0);
+}
+b = read(a, d, letters);
+if (b == -1)
+{
+free(d);
+close(a);
+return (0);
+}
+c = write(STDOUT_FILENO, d, b);
+free(d);
+close(a);
+if (c == -1)
+{
+return (0);
+}
+return (c);
 }
