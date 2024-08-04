94bf1d300ea860961cd0a5f2d116d86f6f450aec
Jonatha32 <jonaperez038@gmail.com>
2024-08-04 19:01:39 +0000

    task 2 - bit mani

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+{
+return (-1);
+}
+return ((n >> index) & 1);
+}
