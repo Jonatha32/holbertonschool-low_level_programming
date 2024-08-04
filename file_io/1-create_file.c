bb4c3686c7c3c52b3ab7064694ce2d210adab859
Jonatha32 <jonaperez038@gmail.com>
2024-08-04 17:25:17 +0000

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+/**
+ * create_file - create file
+ * @filename: parameter
+ * @text_content: parameter
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int a;
+int b;
+int c = 0;
+if (!filename)
+return (-1);
+a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (a == -1)
+return (-1);
+if (text_content)
+{
+while (text_content[c] != '\0')
+c++;
+b = write(a, text_content, c);
+if (b == -1 || b != c)
+{
+close (a);
+return (-1);
+}
+}
+if (close(a) == -1)
+return (-1);
+return (1);
+}
