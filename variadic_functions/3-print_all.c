0d58155ce2117e19a3cd6d62b483b9a4873184c4
Jonatha32 <jonaperez038@gmail.com>
2024-08-08 03:40:01 +0000

    variadic functions print all pending task completed (I think)

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,43 +8,36 @@
 void print_all(const char * const format, ...)
 {
 va_list a;
-unsigned int b = 0;
+int b = 0;
+char *d = "";
 char *c;
-char d;
-int e = 0;
 va_start(a, format);
 while (format && format[b])
 {
-if (e)
-{
-printf(", ");
-}
-e = 1;
 switch (format[b])
 {
 case 'c':
-d = va_arg(a, int);
-printf("%c", d);
-break;
-case 'i':
-printf("%d", va_arg(a, int));
-break;
-case 'f':
-printf("%f", va_arg(a, double));
+printf("%s%c", d, va_arg(a, int));
 break;
 case 's':
 c = va_arg(a, char *);
 if (c == NULL)
-printf("(nil)");
-else
-printf("%s", c);
+c = "(nil)";
+printf("%s%s", d, c);
+break;
+case 'i':
+printf("%s%d", d, va_arg(a, int));
+break;
+case 'f':
+printf("%s%f", d, va_arg(a, double));
 break;
 default:
-e = 0;
-break;
+b++;
+continue;
 }
+d = ", ";
 b++;
 }
-va_end(a);
 printf("\n");
+va_end(a);
 }
