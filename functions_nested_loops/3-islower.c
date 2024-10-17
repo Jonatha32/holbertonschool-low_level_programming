6f2c8a570b97b1952cec930c4832ff9c3dfb350e
Jonatha32 <jonaperez038@gmail.com>
2024-10-17 21:28:56 +0000

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - function
+ * @c: parameter
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
