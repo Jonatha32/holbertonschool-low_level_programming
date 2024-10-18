37fbaa27d15f70f2bee2aa6db6e2d78a5c55115c
Jonatha32 <jonaperez038@gmail.com>
2024-10-18 20:44:33 +0000

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,30 +2,30 @@
 #include "main.h"
 
 /**
- * 
- *
+ * jack_bauer - function
  */
 
 void jack_bauer(void)
 {
-	int hours, minutes;
+	int hours = 0, minutes;
 	int dh1, dh2, dm1, dm2;
 
-	for (hours = 0; hours <= 23; hours++)
+	while (hours <= 23)
 	{
-			dh1 = hours / 10;
-                        dh2 = hours % 10;
-                        _putchar(dh1 + '0');
-                        _putchar(dh2 + '0');
-                        _putchar(':');
-
-		for (minutes = 0; minutes <= 59; minutes++)
-		{
+		dh1 = hours / 10;
+		dh2 = hours % 10;
+		minutes = 0;
+			for (minutes = 0; minutes <= 59; minutes++)
+			{
 			dm1 = minutes / 10;
-                        dm2 = minutes % 10;
-                        _putchar(dm1 + '0');
-                        _putchar(dm2 + '0');
-                        _putchar('\n');
-		}
+			dm2 = minutes % 10;
+			_putchar(dh1 + '0');
+			_putchar(dh2 + '0');
+			_putchar(':');
+			_putchar(dm1 + '0');
+			_putchar(dm2 + '0');
+			_putchar('\n');
+			}
+		hours++;
 	}
 }
