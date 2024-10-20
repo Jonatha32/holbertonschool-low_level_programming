27d5160f34377035262116d188ccf25a73baac7d
Jonatha32 <jonaperez038@gmail.com>
2024-10-20 04:19:39 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function
+ * @a: parameter
+ * @b: parameter
+ * Return: c
+ */
+
+int add(int a, int b)
+
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
