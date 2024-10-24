82aae83a49aa740fb754e5468d04a8a85d574c30
Jonatha32 <jonaperez038@gmail.com>
2024-10-24 16:35:42 +0000

    task 100 without betty

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,32 +9,49 @@
 void print_times_table(int n)
 {
 	int col, fil, dig1, dig2, dig3, result;
-			
+
+	if (n > 15 || n < 0)
+		return;
+	else
 	for (col = 0; col <= n; col++)
 	{
 		for (fil = 0; fil <= n; fil++)
 		{
 			result = col * fil;
 			dig1 = result / 100;
-        		dig2 = (result % 100) / 10;
-        		dig3 = result % 10;
-			if(result < 10)
+			dig2 = (result % 100) / 10;
+			dig3 = result % 10;
+			if (fil == 0)
 			{
-				_putchar(result + '0');	
+				_putchar(result + '0');
 			}
-			else if (result => 10 && result < 100)
+			else if (result < 10)
 			{
-				_putchar(dig1 + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' '); 
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (result >= 10 && result < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(dig2 + '0');
+                                _putchar(dig3 + '0');
 			}
 			else if (result >= 100)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(dig1 + '0');
-				_putchar(dig2 + '0');
-				_putchar(dig3 + '0');
-			}
+                                _putchar(dig2 + '0');
+                                _putchar(dig3 + '0');
 		}
-	}
+		}
+		_putchar('\n');
 	_putchar('\n');
-	
+	}
+
 }
