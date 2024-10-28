7f9300a565116f18fed38799907c3c69c696f0f4
Jonatha32 <jonaperez038@gmail.com>
2024-10-28 13:21:26 +0000

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function
+ * @a: parameter
+ * @b: parameter
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
