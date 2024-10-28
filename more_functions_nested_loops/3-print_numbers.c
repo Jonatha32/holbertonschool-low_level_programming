bfdb2e90e6a53cc4148fb61f981609f37c4bf69a
Jonatha32 <jonaperez038@gmail.com>
2024-10-28 13:26:48 +0000

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - function
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}
