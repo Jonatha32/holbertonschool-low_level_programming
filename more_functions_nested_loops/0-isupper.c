bdc81a9cda6f6a0b33d69cc37eb6c68d2211a0af
Jonatha32 <jonaperez038@gmail.com>
2024-10-28 13:01:35 +0000

    task 0 betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
- *
- *
+ * _isupper - function
+ * @c: parameter
+ * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
-	return 0;
+	return (0);
 }
