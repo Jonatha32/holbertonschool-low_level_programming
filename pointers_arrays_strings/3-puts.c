49429db95d69092ccf610317df40305d5f0ae23d
Jonatha32 <jonaperez038@gmail.com>
2024-10-30 20:33:05 +0000

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function
+ * @str: parameter
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (a != '\0')
+	{
+		a++;
+	}
+	puts(str);
+}
