4461aaf60cfcda9f8da3575e59ba8bb400a5a21c
Jonatha32 <jonaperez038@gmail.com>
2024-10-30 12:28:25 +0000

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- *
+ * print_square - function
+ * @size: parameter
  */
 
 void print_square(int size)
@@ -15,13 +16,13 @@ void print_square(int size)
 	}
 	else
 	{
-		for (a = 0; a <= size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b <= a; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 	}
 }
