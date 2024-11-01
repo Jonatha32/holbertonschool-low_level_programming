4bda58c7bd6b191da6808b9972aed5189e0007f8
Jonatha32 <jonaperez038@gmail.com>
2024-11-01 20:16:42 +0000

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - function
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
