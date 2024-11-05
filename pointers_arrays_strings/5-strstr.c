657d6b54e7b200496efe1840bb3fbb6ad04b04ea
Jonatha32 <jonaperez038@gmail.com>
2024-11-05 15:03:44 +0000

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - function
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: NULL or &needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a] == needle[0])
+			{
+				return (&needle[b]);
+			}
+		}
+	}
+	return (NULL);
+}
