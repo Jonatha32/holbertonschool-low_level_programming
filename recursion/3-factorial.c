0d6a3338adddc6fac4c21f201d32c6c612235c91
Jonatha32 <jonaperez038@gmail.com>
2024-11-08 15:27:06 +0000

    task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+		return (1);
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (0);
+}
