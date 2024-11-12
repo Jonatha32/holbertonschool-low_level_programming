55047df9f872a24b9e6b3a32eab949fb0bc957ef
Jonatha32 <jonaperez038@gmail.com>
2024-11-12 15:32:03 +0000

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,11 +14,16 @@ char *create_array(unsigned int size, char c)
 	char *b;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	b = malloc(size * sizeof(char));
 	if (b == NULL)
 	{
 		printf("failed to allocate memory\n");
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
