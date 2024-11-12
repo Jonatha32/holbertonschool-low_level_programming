1b64a84e3cb1fcc5ec77dfc457b81e5dbab0b6a0
Jonatha32 <jonaperez038@gmail.com>
2024-11-12 16:13:20 +0000

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - function
+ * @str: parameter
+ * Return: s
+ */
+
+char *_strdup(char *str)
+{
+	int a = 0, b;
+	char *s;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	s = malloc((a + 1) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (b = 0; s[b] < a; b++)
+	{
+		s[b] = str[b];
+	}
+	s[a] == '\0';
+	return (s);
+}
