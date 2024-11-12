154936c6faad24a2088db4489a5cc78461bb7fa4
Jonatha32 <jonaperez038@gmail.com>
2024-11-12 13:07:27 +0000

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function
+ * @size: parameter
+ * @c: parameter
+ * Return: b
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *b;
+	unsigned int i;
+
+	b = malloc(size * sizeof(char));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		b[i] = c;
+	}
+
+	return (b);
+}
